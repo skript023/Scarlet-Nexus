3c98143b95e7ba878118d8e1ef5b3ec8645228e0
Putri Novita <79650122+skript023@users.noreply.github.com>
2021-10-08 04:17:09 +0700

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,4 +1,4 @@
 @echo off
 cd "%~dp0\"
-pyinstaller --key 030023 -n "Scarlet Nexus" -F -y --clean -i "C:\dbase\tugas\New folder\Ellohim Project\Ellohim.ico" "%~dp0\scarlet_nexus_trainer.py"
+pyinstaller -n "Scarlet Nexus" -F -y --clean -i "C:\dbase\tugas\New folder\Ellohim Project\Ellohim.ico" "%~dp0\scarlet_nexus_trainer.py"
 pause
